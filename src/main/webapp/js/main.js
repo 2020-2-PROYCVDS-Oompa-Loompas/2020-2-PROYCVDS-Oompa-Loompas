71dbbb23e63491eb08a95dd1e21d54713fc6246b
Juan Alayon <juan.alayon@mail.escuelaing.edu.co>
2020-11-09 22:39:13 -0500

    Se sube el proyecto a organizacion

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,24 @@
+function ocultar(){
+	
+	
+	document.getElementById("segundo").style.display="none";
+	document.getElementById("tercero").style.display="block";
+	document.getElementById("pr").style.display="none";
+	document.getElementById("sg").style.display="block";
+	
+}
+
+function mostrar(){
+	
+	
+	document.getElementById("segundo").style.display="block";
+	document.getElementById("tercero").style.display="none";
+	document.getElementById("sg").style.display="none";
+	document.getElementById("pr").style.display="block";
+	
+}
+
+
+
+
+
